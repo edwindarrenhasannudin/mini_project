0a4537c7466c298678b84f4228e5e5e6e4921445
edwindarrenhasannudin <edwint1j@gmail.com>
2024-11-25 19:56:04 +0700

    memperbaiki error

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroBanner() {
   return (
     <section style={{ textAlign: 'center', padding: '50px 20px', backgroundColor: '#f4f4f4' }}>
-      <h1>Hi, I'm Edwin Darren Hasannudin</h1>
+      <h1>Hi, I&apos;m Edwin Darren Hasannudin</h1>
       <p>Informatics Student at ITERA</p>
     </section>
   )
